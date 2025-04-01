1c2b51a8f2f508d9de3188caf76f85efbba182ee
Roger Tuan <r.tuan@datocms.com>
2025-04-01 15:52:57 -0700

    Fix typescript. Remove radix to simplify demo.

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,17 +1,48 @@
 import { ArticleQuery } from "@/graphql/ArticleQuery.graphql";
 import { executeQuery } from "@datocms/cda-client";
+import { StructuredText } from "react-datocms";
 
 export default async function ArticlePage() {
+
   const result = await executeQuery(ArticleQuery, {
     token: process.env.DATOCMS_GRAPHQL_TOKEN!,
+    excludeInvalid: true,
   });
+
+  const { article } = result;
+
+  if (!article) {
+    return <></>;
+  }
+
+  const { title, content } = article;
 
   return (
     <>
-      <h1>Test</h1>
-      <pre>
-        <code>{JSON.stringify(result, null, 2)}</code>
-      </pre>
+      <h1>{title}</h1>
+      {!!content?.blocks && (
+        <StructuredText
+          data={content}
+          renderInlineRecord={(ctx) => {
+            switch (ctx.record.__typename) {
+              case "FaqModelRecord":
+                return <></>;
+
+              default:
+                return null;
+            }
+          }}
+          renderBlock={(ctx) => {
+            switch (ctx.record.__typename) {
+              case "TableBlockRecord":
+                return <h3>Hello!</h3>;
+
+              default:
+                return null;
+            }
+          }}
+        />
+      )}
     </>
   );
 }
--- graphql/ArticleQuery.graphql.ts
+++ graphql/ArticleQuery.graphql.ts
@@ -1,44 +1,119 @@
 import { datoGraphql } from "@/graphql/graphql-lib";
 
-export const ArticleQuery = datoGraphql(
-  `
-    query MyQuery {
-    article(locale: en) {
+const TableFragment = datoGraphql(
+    //language=gql
+    `
+        fragment TableFragment on TableBlockRecord @_unmask {
+            __typename
+            id
+            table
+            caption
+        }`,
+);
+
+const ButtonFragment = datoGraphql(
+    //language=gql
+    `fragment ButtonFragment on ButtonRecord @_unmask {
+        __typename
         id
-        title
-        slug
-        content {
-            value
-            blocks {
-                ... on TableBlockRecord {
-                    __typename
-                    id
-                    table
-                    caption
-                }
+        label
+        primary
+        url
+    }
+    `
+)
+
+const MediaFragment = datoGraphql(
+    //language=gql
+    `fragment MediaFragment on ImageBlockRecord @_unmask {
+        __typename
+        id
+        image {
+            id
+            title
+            thumbhash
+            tags
+            smartTags
+            md5
+            mimeType
+            alt
+            author
+            basename
+            blurUpThumb
+            blurhash
+            copyright
+            exifInfo
+            customData
+            format
+            height
+            width
+            responsiveImage {
+                src
+                srcSet
+                sizes
+                base64
+                bgColor
+                aspectRatio
+                height
+                width
             }
-            links {
-                ... on FaqModelRecord {
-                    id
-                    faq {
-                        __typename
-                        id
-                        question
-                        answer {
-                            value
-                            blocks {
-                                ... on TableBlockRecord {
-                                    __typename
-                                    id
-                                    table
-                                    caption
-                                }
-                            }
-                        }
-                    }
-                }
+            video {
+                thumbnailUrl
+                thumbhash
+                streamingUrl
+                title
+                width
+                height
+                blurUpThumb
+                blurhash
+                alt
             }
         }
     }
-}`,
+    `
+)
+
+export const ArticleQuery = datoGraphql(
+  //language=gql
+  `
+      query MyQuery {
+          article {
+              id
+              title
+              slug
+              content {
+                  value
+                  blocks {
+                      ...TableFragment
+                      ...ButtonFragment
+                      ...MediaFragment
+                  }
+                  links {
+                      ... on FaqModelRecord {
+                          __typename
+                          id
+                          faq {
+                              __typename
+                              id
+                              question
+                              answer {
+                                  value
+                                  blocks {
+                                      ...TableFragment
+                                  }
+                              }
+                          }
+                      }
+                      
+                      ... on ArticleRecord {
+                          __typename
+                          id
+                          slug
+                          title
+                      }
+                  }
+              }
+          }
+      }`, [TableFragment, ButtonFragment, MediaFragment],
 );
+
