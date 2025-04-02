d51d2ad91a3a10ccd8e13bc22d0bd7551b283b54
Roger Tuan <r.tuan@datocms.com>
2025-04-02 08:28:26 -0700

    Better comments

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -19,20 +19,21 @@ import { render as toPlainText } from "datocms-structured-text-to-plain-text";
 import slugify from "@sindresorhus/slugify";
 import { createElement } from "react";
 
-type JsonTable = {
-  columns: string[];
-  data: Array<{
-    [key: string]: string;
-  }>;
-};
-
+// Default Next export that generates a page
 export default async function ArticlePage({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
+  // Get the slug from the URL
   const { slug } = await params;
 
+  // Query the Dato API based on the slug. The full query is in /graphql/ArticleQuery.graphql.ts
+
+  // This whole executeQuery thing is from our beta CDA client (https://www.npmjs.com/package/@datocms/cda-client)
+  // And combined with gql.tada for GraphQL typescript support (https://gql-tada.0no.co/)
+  // If you don't care about all that, you can also just make a simple fetch() request directly
+  // GraphQL API docs here: https://www.datocms.com/docs/content-delivery-api
   const result = await executeQuery(ArticleQuery, {
     token: process.env.DATOCMS_GRAPHQL_TOKEN!,
     excludeInvalid: true,
@@ -43,58 +44,22 @@ export default async function ArticlePage({
 
   const { article } = result;
 
+  // 404 if invalid slug
   if (!article) {
     notFound();
   }
 
+  // This is all fully (and automatically) typed because of gql.tada!
   const { title, content } = article;
 
-  const customNodeRules: RenderRule<TrasformFn, TrasformFn, TrasformFn>[] = [
-    // Add HTML anchors to heading levels for in-page navigation
-    renderNodeRule(isHeading, ({ node, children, key }) => {
-      const HeadingTag = `h${node.level}`;
-      const slug = slugify(toPlainText(node)!) ?? key;
-
-      return createElement(
-        "a",
-        {
-          key,
-          href: `#${slug}`,
-        },
-        createElement(
-          HeadingTag,
-          {
-            id: slug,
-          },
-          children,
-        ),
-      );
-    }),
-
-    // Workaround for structured text bug where everything is a p tag by default and can't nest other things
-    renderNodeRule(
-      isParagraph,
-      ({ adapter: { renderNode }, children, key, node }) => {
-        if (hasChildren(node)) {
-          return (
-            <div key={key} className={"fake-paragraph"}>
-              {children}
-            </div>
-          );
-        } else {
-          // Proceed with default paragraph rendering...
-          return renderNode("p", { key }, children);
-        }
-      },
-    ),
-  ];
-
+  // The actual rendering logic for this page
   return (
     <>
       <h1>{title}</h1>
       {!!content && (
         <StructuredText
-          data={content}
+          data={content} // This is in a "DAST" format, a JSON-based syntax tree: https://www.datocms.com/blog/how-to-use-datocms-structured-text-field-in-a-nextjs-app
+          // This defines how we render a particular type of inlined record, in this case a reusable FAQ entry
           renderInlineRecord={(ctx) => {
             switch (ctx.record.__typename) {
               case "FaqModelRecord":
@@ -114,18 +79,24 @@ export default async function ArticlePage({
                   </>
                 );
 
+              // In a real page, you would account for every possible inlined record type
+              // But for this demo, we just skip over anything we don't know how to handle
               default:
                 return null;
             }
           }}
-          renderBlock={blockRenderer}
-          customNodeRules={customNodeRules}
+          renderBlock={blockRenderer} // Similarly, this is how we render blocks. In its own function because multiple <StructuredTexts/> can reuse the same blocks.
+          customNodeRules={customNodeRules} // We use this to override some default renderers for headings and paragraph tags
         />
       )}
     </>
   );
 }
 
+// This queries our API and lets Next pre-build all the articles
+// For the demo, in the global layout.tsx, we set a revalidate=1 so every page gets checked at most once a second
+// For prod, you'd normally use a value like revalidate=60 (1 min) or use cache tags or webhooks to invalidate
+// See https://nextjs.org/docs/app/building-your-application/data-fetching/incremental-static-regeneration
 export async function generateStaticParams() {
   const result = await executeQuery(AllArticlesQuery, {
     token: process.env.DATOCMS_GRAPHQL_TOKEN!,
@@ -137,10 +108,13 @@ export async function generateStaticParams() {
   }));
 }
 
+// A reusable set of rendering rules that tells StructuredText how to render your custom blocks
 const blockRenderer = (
   ctx: RenderBlockContext<StructuredTextGraphQlResponseRecord>,
 ) => {
   switch (ctx.record.__typename) {
+    // The tables plugin gives us back JSON and we can render that into whatever we want, including HTML tables or cards
+    // In production you'd probably want to make something responsive and mobile-friendly
     case "TableBlockRecord":
       const { table } = ctx.record;
       const { columns, data } = table as JsonTable;
@@ -170,6 +144,7 @@ const blockRenderer = (
         </table>
       );
 
+    // For this demo, this warns you when you have an unhandled block type
     default:
       return (
         <>
@@ -183,3 +158,54 @@ const blockRenderer = (
       );
   }
 };
+
+// Custom output type for the tables plugin that Theresa used: https://www.datocms.com/marketplace/plugins/i/datocms-plugin-table-editor
+type JsonTable = {
+  columns: string[];
+  data: Array<{
+    [key: string]: string;
+  }>;
+};
+
+// Custom rendering rules for the Structured Text Fields
+const customNodeRules: RenderRule<TrasformFn, TrasformFn, TrasformFn>[] = [
+  // Add clickable IDs (in-page anchors) to heading levels for in-page navigation
+  renderNodeRule(isHeading, ({ node, children, key }) => {
+    const HeadingTag = `h${node.level}`;
+    const slug = slugify(toPlainText(node)!) ?? key;
+
+    return createElement(
+      "a",
+      {
+        key,
+        href: `#${slug}`,
+      },
+      createElement(
+        HeadingTag,
+        {
+          id: slug,
+        },
+        children,
+      ),
+    );
+  }),
+
+  // Workaround for a cosmetic Structured Text bug where everything is a p tag by default and can't semantically nest other tags
+  // Without this, the page still works but may cause a hydration error with certain Next configs
+  renderNodeRule(
+    isParagraph,
+    ({ adapter: { renderNode }, children, key, node }) => {
+      // If the node has children, we render it as a <div> with margins instead of a <p>
+      if (hasChildren(node)) {
+        return (
+          <div key={key} className={"fake-paragraph"}>
+            {children}
+          </div>
+        );
+      } else {
+        // Or else just render it as a regular <p>
+        return renderNode("p", { key }, children);
+      }
+    },
+  ),
+];
